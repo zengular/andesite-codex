5af2af12406b7a0c1f6355835466bac4374b8f73
laborci <gergely@laborci.hu>
2020-07-01 20:04:47 +0200

    crop

---
+++ admin/image-editor.modal.js
@@ -0,0 +1,48 @@
+import {modalify} from "zengular-ui";
+import {Brick}    from "zengular";
+import twig       from "./image-editor.twig";
+import {Ajax}     from "zengular-util";
+import "./image-editor.scss";
+import Croppr     from "croppr";
+import "croppr/dist/croppr.min.css"
+
+@modalify()
+@Brick.register('codex-image-editor', twig)
+@Brick.registerSubBricksOnRender()
+@Brick.renderOnConstruct(false)
+export default class CodexImageEditor extends Brick {
+
+	beforeRender(args) {
+		console.log(args)
+		this.img = args.url;
+	}
+
+	createViewModel() {
+		return {img: this.img}
+	}
+
+	onRender() {
+		this.$$('close').on.click(()=>this.close(false));
+		this.$$('ratio').on.input((event, target)=>{
+			if(target.checkValidity()){
+				let ratio = parseInt(target.value.split(':')[1]) / parseInt(target.value.split(':')[0])
+				this.cropInstance.options.aspectRatio = ratio;
+			}else{
+				this.cropInstance.options.aspectRatio = null;
+				this.cropInstance.reset();
+			}
+		})
+		this.$$('save').on.click(()=>{
+			this.close(this.cropInstance.getValue())
+		});
+		this.$$('croppr').listen('load', (event, target)=>{
+			this.cropInstance = new Croppr(this.$$('croppr').node, {
+				onCropMove: (value)=>{
+					this.$$('width').node.innerHTML = value.width;
+					this.$$('height').node.innerHTML = value.height;
+				}
+			});
+			this.cropInstance.reset();
+		})
+	}
+}
