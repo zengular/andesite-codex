d48f56d8f277c63df50c13fb52b65aeb13ef00e3
laborci <gergely@laborci.hu>
2020-09-09 05:01:32 +0200

    code input added

---
+++ admin/inputs/code.brick.js
@@ -0,0 +1,17 @@
+import AbstractInputBrick from "zengular-codex/admin/inputs/~abstract-input-brick";
+import {Brick}            from "zengular";
+import twig               from "./code.twig";
+import "./code.less";
+import * as  indentation  from 'indent-textarea';
+
+@Brick.register('codex-input-code', twig)
+@Brick.registerSubBricksOnRender()
+
+export default class InputCode extends AbstractInputBrick {
+	getDefaultOptions() { return {rows: 5}}
+	getValue() { return this.$$("input").node.value;}
+	setValue(value) {this.$$("input").node.value = value;}
+	onRender() {
+		indentation.watch(this.$$("input").node);
+	}
+}
